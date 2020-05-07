6226441928ba4dd2feeabdfc88fb80e2b56fb897
Adriano Avelino <adriano-st777@hotmail.com>
2020-05-07 10:53:13 -0300

    adiciona um teste de update

--- src/main/java/br/estudy/hibernate/ja/Teste.java
+++ src/main/java/br/estudy/hibernate/ja/Teste.java
@@ -1,5 +1,6 @@
 package br.estudy.hibernate.ja;
 
+import br.estudy.hibernate.connection.ConnectionFactory;
 import br.estudy.hibernate.dao.CategoriaDao;
 import br.estudy.hibernate.model.Categoria;
 
@@ -8,10 +9,13 @@ public class Teste {
         System.out.println("Olaaa");
         
         Categoria c = new Categoria();
-        c.setDescricao("categoria dao");
+        c.setId(9);
+        c.setDescricao("bebidax");
         
         CategoriaDao dao = new CategoriaDao();
         dao.save(c);
+        System.out.println(c);
+        ConnectionFactory.emf.close();
     }
     
 }
